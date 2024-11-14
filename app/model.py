ddb4b90d1bf9e53b2579dfaaf1a3c47ce178eb5f
salim <salimbekkari1998@gmail.com>
2024-11-14 12:22:17 +0100

    models updeted

--- app/model.py
+++ app/model.py
@@ -8,6 +8,10 @@ class User(db.Model):
     last_name = db.Column(db.String(50), nullable=False)
     birth_date = db.Column(db.Date)
     properties = db.relationship('Property', backref='owner', lazy=True)
+    def __init__(self, first_name, last_name, birth_date):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_date = birth_date
 
 # Modèle de bien immobilier
 class Property(db.Model):
@@ -19,7 +23,12 @@ class Property(db.Model):
     city = db.Column(db.String(50))
     owner_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     rooms = db.relationship('Room', backref='property', lazy=True)
-
+    def __init__(self, name, description, type, city, owner_id):
+        self.name = name
+        self.description = description
+        self.type = type
+        self.city = city
+        self.user_id = owner_id
 # Modèle de pièce
 class Room(db.Model):
     __tablename__ = 'rooms'
@@ -27,3 +36,8 @@ class Room(db.Model):
     property_id = db.Column(db.Integer, db.ForeignKey('properties.id'), nullable=False)
     name = db.Column(db.String(50))
     size = db.Column(db.Float)
+
+    def __init__(self, name, area, property_id):
+        self.name = name
+        self.area = area
+        self.property_id = property_id
